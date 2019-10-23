fad009a03698829b00768673790752b8c548a4a3
harshkeshwala <keshwalaharsh@gmail.com>
2019-10-23 12:28:22 -0400

    [Project created and Add-contact modulde created]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
